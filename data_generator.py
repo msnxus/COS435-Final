1149519f0458f417620bca465dc5a19307ee5df9
Ryan Hoffman <rh8490@princeton.edu>
2025-04-20 22:00:39 -0400

    added comments to data generator

--- data_generator.py
+++ data_generator.py
@@ -3,6 +3,12 @@ from torch.distributions import Beta
 from scipy.sparse.csgraph import shortest_path
 
 # Synthetic data generation for TSP solver
+# Creates an (i, n, k) tensor which represents the time to get from i->j at a timestep k
+# when i=j, the value is inf
+# For all i,j pairs, a random Beta curve is created (alpha and beta on 0.5 - 4.5)
+# For all k in that i,j trajectory, the PDF value of the curve is taken and noise is added
+# Therefore trajectories follow a basic structure but are still randomized
+
 def generate(n_destinations: int, n_timesteps: int) -> torch.Tensor:
     noise_scale = 0.5
     noise_mean = 5
@@ -25,7 +31,8 @@ def generate(n_destinations: int, n_timesteps: int) -> torch.Tensor:
 
     return y + noise
 
-# Apply bellman ford to each timestep of the synthetic data
+# Apply bellman ford to each timestep of the synthetic data and return as a tensor
+# im worried this might change the shape shape of the tensor to be the wrong order of dims
 def shortest_paths_by_slice(a: torch.Tensor) -> torch.Tensor:
     optim_slices = []
     for k in range(a.shape[2]):
